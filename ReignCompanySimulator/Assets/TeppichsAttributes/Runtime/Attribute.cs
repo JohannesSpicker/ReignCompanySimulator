db28c9a702166df930c6614133a8bf12b5e14799
Teppich <johannesspicker@me.com>
2022-11-24 09:49:53 +0100

    Added AttributeData

--- ReignCompanySimulator/Assets/TeppichsAttributes/Runtime/Attribute.cs
+++ ReignCompanySimulator/Assets/TeppichsAttributes/Runtime/Attribute.cs
@@ -9,4 +9,13 @@ namespace TeppichsAttributes
         
         
     }
+
+    [Serializable]
+    public class AttributeData
+    {
+        [SerializeField] public string inGameName;
+        [SerializeField] public bool usesMaxValue = false;
+        [SerializeField] public float maxValue = 0f;
+        
+    }
 }
