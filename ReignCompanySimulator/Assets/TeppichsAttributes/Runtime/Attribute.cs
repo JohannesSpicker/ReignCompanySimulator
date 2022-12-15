c7285d69fdf003873ca6a47fd998d44e2a151a12
teppich <johannesspicker@me.com>
2022-12-15 02:19:01 +0100

    Refined attributes

--- ReignCompanySimulator/Assets/TeppichsAttributes/Runtime/Attribute.cs
+++ ReignCompanySimulator/Assets/TeppichsAttributes/Runtime/Attribute.cs
@@ -1,38 +1,154 @@
 ﻿using System;
 using System.Collections.Generic;
-using Sirenix.OdinInspector;
+using System.Linq;
 using TeppichsAttributes.Runtime;
-using UnityEngine;
 
 namespace TeppichsAttributes
 {
-    public class Attribute 
+    public abstract class Attribute
     {
-        private AttributeData data;
-        private float         baseValue;
-
-        [SerializeField, ReadOnly]
-        private float currentValue;
-
-        private List<Modifier> modifiers = new();
+        protected float         baseValue;
+        private   AttributeData data;
 
         public Attribute(AttributeData data, float baseValue)
         {
             this.data      = data;
             this.baseValue = baseValue;
+            Value          = baseValue;
         }
 
-        protected Attribute() { throw new NotImplementedException(); }
+        #region Value
+
+        public float Value { get; protected set; }
+
+        public event Action<float> OnAttributeValueChanged;
+        public event Action<float> OnAttributeValueChangedByAmount;
+
+        protected void InvokeOnAttributeValueChanged() => OnAttributeValueChanged?.Invoke(Value);
+
+        protected void InvokeOnAttributeValueChangedByAmount(float before) =>
+            OnAttributeValueChangedByAmount?.Invoke(Value - before);
+
+        #endregion
     }
 
-    public abstract class DerivedAttribute : Attribute
+    public class StatAttribute : Attribute
+    {
+        protected List<Modifier> modifiers = new();
+
+        public StatAttribute(AttributeData data, float baseValue) : base(data, baseValue) { }
+
+        #region Modifiers
+
+        public void AddModifier(Modifier modifier)
+        {
+            modifiers.Add(modifier);
+            RecalculateValue();
+        }
+
+        public void RemoveModifier(Modifier modifier)
+        {
+            modifiers.Remove(modifier);
+            RecalculateValue();
+        }
+
+        public void RemoveAllModifiersFromSource(object source)
+        {
+            modifiers.RemoveAll(mod => mod.source == source);
+            RecalculateValue();
+        }
+
+        protected void RecalculateValue()
+        {
+            float before = Value;
+
+            Value = CalculateFinalValue();
+
+            InvokeOnAttributeValueChanged();
+            InvokeOnAttributeValueChangedByAmount(before);
+
+            float CalculateFinalValue()
+            {
+                float finalValue = baseValue;
+
+                finalValue += modifiers.Where(mod => mod.type == ModifierType.Flat).Sum(mod => mod.value);
+                finalValue *= 1 + modifiers.Where(mod => mod.type == ModifierType.PercentAdd).Sum(mod => mod.value);
+
+                foreach (Modifier mod in modifiers.Where(s => s.type == ModifierType.PercentMult))
+                    finalValue *= 1 + mod.value;
+
+                return finalValue;
+            }
+        }
+
+        #endregion
+    }
+
+    public abstract class DerivedStatAttribute : StatAttribute
     {
         //refers to attributes it derives from
         //gets baseValue via the related attributes, then adds its own modifiers
+
+        private List<StatAttribute> factors = new();
+
+        
+        //need to get the concrete attributes into here for calculations
+        protected DerivedStatAttribute(DerivedAttributeData data, float baseValue, List<StatAttribute> factors) :
+            base(data, baseValue)
+        {
+            
+            
+        }
+        
+        //when recalculating value with local modifiers use the values of the factors as base (just multiply them for now)
     }
 
+    /// <summary>
+    ///     modifiers are added immediately, not stored
+    /// </summary>
     public class Resource : Attribute
     {
-        //modifiers are added immediately, not stored
+        public Resource(AttributeData data, float baseValue) : base(data, baseValue) { }
+
+        public bool TrySpend(float amount)
+        {
+            if (Value < amount)
+                return false;
+
+            AlterValue(-amount);
+
+            return true;
+        }
+
+        public void Gain(float  amount) => AlterValue(amount);
+        public void Spend(float amount) => AlterValue(-amount);
+
+        public void ModifyValue(Modifier modifier)
+        {
+            if (modifier.type is ModifierType.Flat)
+                AlterValue(modifier.value);
+            else if (modifier.type is ModifierType.PercentAdd or ModifierType.PercentMult)
+                MultiplyValue(modifier.value);
+        }
+
+        private void AlterValue(float amount)
+        {
+            float before = Value;
+
+            Value += amount;
+
+            InvokeOnAttributeValueChanged();
+            InvokeOnAttributeValueChangedByAmount(before);
+        }
+
+        private void MultiplyValue(float factor)
+        {
+            float before = Value;
+
+            Value *= factor;
+
+            InvokeOnAttributeValueChanged();
+            InvokeOnAttributeValueChangedByAmount(before);
+        }
     }
 }
--- ReignCompanySimulator/Assets/TeppichsAttributes/Runtime/AttributeData.cs
+++ ReignCompanySimulator/Assets/TeppichsAttributes/Runtime/AttributeData.cs
@@ -1,20 +1,27 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace TeppichsAttributes
 {
     /// <summary>
-    /// This is a flyweight.
+    ///     This is a flyweight.
     /// </summary>
     [Serializable]
     public class AttributeData : ScriptableObject
     {
-        [SerializeField] public  string inGameName;//exchange for localizedString
+        [SerializeField] public  string inGameName; //TODO: exchange for localizedString
         [SerializeField] private Sprite sprite;
 
         [SerializeField] public bool  usesMaxValue;
         [SerializeField] public float maxValue;
         [SerializeField] public bool  usesMinValue;
         [SerializeField] public float minValue;
+    }
+
+    [Serializable]
+    public class DerivedAttributeData : AttributeData
+    {
+        [SerializeField] private List<AttributeData> factors = new();
     }
 }
